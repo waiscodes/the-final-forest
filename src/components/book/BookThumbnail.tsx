38f7c7c0e34a474b0ed117e9666cd64ebd17954e
waiscodes <birm@ualberta.ca>
2022-02-20 22:12:15 -0700

    created book thumbnail and book review components

---
+++ src/components/book/BookThumbnail.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BookThumbnail = () => {
+  return (
+    <div className='book-thumbnail'>
+      <img src="" alt="" />
+      <div>
+        5 stars
+      </div>
+    </div>
+  )
+}
+
+export default BookThumbnail
---
+++ src/components/book/BookWithReview.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const BookWithReview = () => {
+  return (
+    <div className='book-with-review'>
+      <div>
+        {/* <img src="" alt="" /> */}
+      </div>
+      <div>
+        <div className="movie-title">The Batman <span>2022</span></div>
+        <div>Watched by username 19 Feb 2022</div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque 
+          iusto perspiciatis beatae rerum tempora animi odit ullam est veniam neque.
+        </div>
+        <div>420 likes</div>
+      </div>
+    </div>
+  )
+}
+
+export default BookWithReview
